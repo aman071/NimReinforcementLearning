2ef6acca88c00d84fd7eed099aa9b0cb27b174a2
aman071 <amanrehman18@gmail.com>
2020-10-06 18:06:31 +0530

    get q val and update start

--- nim.py
+++ nim.py
@@ -101,7 +101,12 @@ class NimAI():
         Return the Q-value for the state `state` and the action `action`.
         If no Q-value exists yet in `self.q`, return 0.
         """
-        raise NotImplementedError
+        q_value=self.q[state, action]
+
+        if q_value==None:
+            return 0
+        
+        return q_value
 
     def update_q_value(self, state, action, old_q, reward, future_rewards):
         """
@@ -118,7 +123,9 @@ class NimAI():
         `alpha` is the learning rate, and `new value estimate`
         is the sum of the current reward and estimated future rewards.
         """
-        raise NotImplementedError
+        new_value_estimate=reward + future_rewards
+
+        self.q[state,action] = old_q + self.alpha*()
 
     def best_future_reward(self, state):
         """
